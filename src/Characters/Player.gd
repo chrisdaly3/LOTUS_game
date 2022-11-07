f06a65e2514d34a0a0965a04b349167aa1966cb5
Chris Daly <cdaly3.exe@gmail.com>
2022-11-07 16:13:08 -0500

    Implements Aim-by-Mouse Mechanic for Sonar

--- src/Characters/Player.gd
+++ src/Characters/Player.gd
@@ -1,45 +1,37 @@
-extends Area2D
+extends KinematicBody2D
 
 var speed = 95
-var screen_size
+var velocity = Vector2()
+var sonar = preload("res://src/Player_Tools/Sonar.tscn")
 
-# sends var to inspector for use:
-export(PackedScene) var echo
-
-# gets size of screen, and hides character until ready for use.
-func _ready():
-	screen_size = get_viewport_rect().size
-	# hide()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO #set player's starting movement vector
-	if Input.is_action_pressed("walk_right"):
-		velocity.x += 1
+func user_input():
+	# accepts user input to move throughout the level
+	velocity = Vector2()
 	if Input.is_action_pressed("walk_left"):
 		velocity.x -= 1
+	if Input.is_action_pressed("walk_right"):
+		velocity.x += 1
 	if Input.is_action_pressed("walk_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("walk_up"):
 		velocity.y -= 1
-		
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play("walk")
 	else:
 		$AnimatedSprite.stop()
-		
-	# update player position with animations	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	
-	#flip character image depending on direction (left or right)
-	$AnimatedSprite.flip_h = velocity.x < 0
-	
+
+func _process(delta):
+	user_shout()
+	$Aimer.rotation = get_angle_to(get_global_mouse_position())
+
+func _physics_process(delta):
+	user_input()
+	velocity = move_and_slide(velocity)
+
+func user_shout():
 	if Input.is_action_just_pressed("shout"):
-		shout()
-	
-func shout():
-	var e = echo.instance()
-	owner.add_child(e)
-	e.transform = global_transform
+		var sonar_instance = sonar.instance()
+		sonar_instance.start($Aimer.global_position, $Aimer.rotation)
+		owner.add_child(sonar_instance)
--- src/Player_Tools/Sonar.gd
+++ src/Player_Tools/Sonar.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
-var speed = 70
+var speed = 120
+var velocity = Vector2()
 
-var velocity = Vector2(speed,0)
-
+func start(pos, dir):
+	rotation = dir
+	position = pos
+	velocity = Vector2(speed, 0).rotated(rotation)
+	 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
